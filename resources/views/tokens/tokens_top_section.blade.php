25573c59f7dede5bf592ca4b9fd2377c94923c52
Sevikon DELL <sevikon@gmail.com>
2018-05-31 01:04:51 +0200

    mobile version added

--- resources/views/tokens/tokens_top_section.blade.php
+++ resources/views/tokens/tokens_top_section.blade.php
@@ -7,18 +7,16 @@
                 @include('tokens.tokens_coin',['cl' => 'coin-3'])
                 @include('tokens.tokens_coin',['cl' => 'coin-4'])
             </div>
-            <div class="content">
+            <div class="row content">
                 <h1 class="title">@lang('tokens.tokens_title')</h1>
                 <p class="desc desc-1">@lang('tokens.tokens_sub')</p>
                 <p class="desc desc-2">@lang('tokens.tokens_sub_2')</p>
             </div>
-            <div class="col-md-12 center">
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <div class="counter-section">
-                            <h1 class="title">@lang('tokens.counter_title')</h1>
-                            @include('home.countdown')
-                        </div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="counter-section">
+                        <h1 class="title">@lang('tokens.counter_title')</h1>
+                        @include('home.countdown')
                     </div>
                 </div>
             </div>
