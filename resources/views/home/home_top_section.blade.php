986ce4dc5c51c2c7e2a9d07013e76d380a228c55
Sevikon DELL <sevikon@gmail.com>
2018-05-31 22:23:24 +0200

    add video

--- resources/views/home/home_top_section.blade.php
+++ resources/views/home/home_top_section.blade.php
@@ -1,70 +1,73 @@
 <section class="home-top-section top-section center">
-    <div class="container">
-        <div class="center">
-            <div class="row">
-                <div class="col-md-6 col-md-push-6">
-                    <img src="/images/custom/token-sale/vector-smart-object.png"
-                         srcset="/images/custom/token-sale/vector-smart-object@2x.png 2x,
-             /images/custom/token-sale/vector-smart-object@3x.png 3x"
-                         class="top-map-image">
-                </div>
-                <div class="col-md-6 col-md-pull-6">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="top-title">@lang('home.top_title')</div>
-                            <div class="play play-button" data-video-src="Qbu_FRg8vuU"
-                                 data-target="#videoModal"
-                                 id="playVideoButton">
-                                <div class="play-left">
-                                    <img src="/images/custom/token-sale/play-icon.png"
-                                         srcset="/images/custom/token-sale/play-icon@2x.png 2x,
+    <img src="/video/preview_mobile.jpg"
+         srcset="/video/preview_mobile.jpg,
+             /video/preview_mobile.jpg"
+         class="img-video-top">
+    <div class="video-bg">
+        <video loop muted autoplay poster="/video/preview_header.jpg" class="video-bg_video">
+            <source src="/video/paket_header.mp4" type="video/mp4">
+            <source src="/video/paket_header.ogv" type="video/ogg">
+            <source src="/video/paket_header.webm" type="video/webm">
+        </video>
+    </div>
+    <div class="container center">
+        <div class="row home-top-content">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="top-title">@lang('home.top_title')</div>
+                        <div class="play play-button" data-video-src="Qbu_FRg8vuU"
+                             data-target="#videoModal"
+                             id="playVideoButton">
+                            <div class="play-left">
+                                <img src="/images/custom/token-sale/play-icon.png"
+                                     srcset="/images/custom/token-sale/play-icon@2x.png 2x,
              /images/custom/token-sale/play-icon@3x.png 3x"
-                                         class="play-icon">
-                                </div>
-                                <div class="play-right">
-                                    @lang('home.play_video')
-                                </div>
-
+                                     class="play-icon">
                             </div>
+                            <div class="play-right">
+                                @lang('home.play_video')
+                            </div>
+
                         </div>
-                        <div class="col-md-12">
-                            <div class="top-desc">@lang('home.top_desc')</div>
-                        </div>
-                        <div class="col-md-12 top-actions">
-                            <a href="/documents/wp/en.pdf" target="_blank">
-                                <div class="btn-top-action btn-top-action-1">
-                                    <img src="/images/custom/token-sale/pdf-icon.png"
-                                         srcset="/images/custom/token-sale/pdf-icon@2x.png 2x,
+                    </div>
+                    <div class="col-md-12">
+                        <div class="top-desc">@lang('home.top_desc')</div>
+                    </div>
+                    <div class="col-md-12 top-actions">
+                        <a href="/documents/wp/en.pdf" target="_blank">
+                            <div class="btn-top-action btn-top-action-1">
+                                <img src="/images/custom/token-sale/pdf-icon.png"
+                                     srcset="/images/custom/token-sale/pdf-icon@2x.png 2x,
              /images/custom/token-sale/pdf-icon@3x.png 3x"
-                                         class="pdf-icon">
-                                    @lang('home.top_action_wp')
-                                </div>
-                            </a>
-                            <a href="https://t.me/PaketCommunity" target="_blank">
-                                <div class="btn-top-action btn-top-action-2">
-                                    <img src="/images/custom/token-sale/logo-copy.png"
-                                         srcset="/images/custom/token-sale/logo-copy@2x.png 2x,
+                                     class="pdf-icon">
+                                @lang('home.top_action_wp')
+                            </div>
+                        </a>
+                        <a href="https://t.me/PaketCommunity" target="_blank">
+                            <div class="btn-top-action btn-top-action-2">
+                                <img src="/images/custom/token-sale/logo-copy.png"
+                                     srcset="/images/custom/token-sale/logo-copy@2x.png 2x,
              /images/custom/token-sale/logo-copy@3x.png 3x"
-                                         class="logo-copy">
-                                    @lang('home.top_action_telegram')
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-12">
-                            <a href="https://identity.paket.global/" target="_blank">
-                                <div class="btn-join">
-                                    @lang('home.top_join_now')
-                                </div>
-                            </a>
-                        </div>
+                                     class="logo-copy">
+                                @lang('home.top_action_telegram')
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-12">
+                        <a href="https://identity.paket.global/" target="_blank">
+                            <div class="btn-join">
+                                @lang('home.top_join_now')
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-12 center">
-            <div class="row">
+    </div>
+    <div class="col-md-12 center">
+        <div class="row">
 
-            </div>
         </div>
     </div>
 </section>
