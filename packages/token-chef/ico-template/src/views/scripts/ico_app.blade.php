e77a2ea52453cbae452b6e8a86e0f4d3cdff7388
Sevikon DELL <sevikon@gmail.com>
2018-06-01 15:29:38 +0200

    fix cookies cdn

--- packages/token-chef/ico-template/src/views/scripts/ico_app.blade.php
+++ packages/token-chef/ico-template/src/views/scripts/ico_app.blade.php
@@ -1,5 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/js-cookie@2.2.0/src/js.cookie.min.js"
-        integrity="sha256-OmJjYTJ9dwqmxpiE1XO04BTAzDezSs69LWyQpMz1cZE="
+<script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.2.0/js.cookie.min.js"
+        integrity="sha384-ujpAYcyxFaJsZN5668lLgOpEH8vtWrbOq8fvj+WZ2kD71LJwGa/9QP/suPPF1hTI"
         crossorigin="anonymous"></script>
 
 <script data-cfasync="false" src="/vendor/ico-template/js/client_app.js?{{str_random(10)}}"></script>
