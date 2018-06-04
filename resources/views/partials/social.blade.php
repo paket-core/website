ecac239b386eebc14d73f1266273ca6ddd5bbf6a
Sevikon DELL <sevikon@gmail.com>
2018-06-04 17:24:32 +0200

    change telegram group name

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -2,5 +2,5 @@
     <a href="https://twitter.com/PaketProtocol" target="_blank"><span class="icon-twitter"></span></a>
     <a href="https://www.reddit.com/r/PAKET/" target="_blank"><span class="icon-reddit"></span></a>
     <a href="https://medium.com/@PaketProtocol" target="_blank"><span class="icon-medium"></span></a>
-    <a href="https://t.me/PaketCommunity" target="_blank"><span class="icon-telegram"></span></a>
+    <a href="https://t.me/joinchat/G9T-tkXBHHNnpQ8oYFGSvw" target="_blank"><span class="icon-telegram"></span></a>
 </div>
