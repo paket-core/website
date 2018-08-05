e94b6b769443b0db3d8c9247a446d43a0cc397a6
Borxes <yurasherman+github@gmail.com>
2018-08-05 18:42:17 +0300

    update Medium URL

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,7 +1,7 @@
 <div class="social-icons">
     <a href="https://twitter.com/PaketProtocol" target="_blank"><span class="icon-twitter"></span></a>
     <a href="https://www.reddit.com/r/PAKET/" target="_blank"><span class="icon-reddit"></span></a>
-    <a href="https://medium.com/@PaketProtocol" target="_blank"><span class="icon-medium"></span></a>
+    <a href="https://medium.com/paket-global" target="_blank"><span class="icon-medium"></span></a>
     <a href="{{\TokenChef\IcoTemplate\Services\StaticArray::TELEGRAM_URL}}" target="_blank"><span class="icon-telegram"></span></a>
     <a href="https://www.youtube.com/channel/UCeyTJH3uKlXsEyD-vPRQ7uw" target="_blank"><span class="icon-yt"></span></a>
 </div>
