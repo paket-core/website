5efe589b0b1e355c84c184f61d3db118e75598b8
YuriyKondrat <yuriy.k@it-dimension.com>
2018-10-09 16:42:35 +0300

    test js translate

---
+++ translate/js/translate/dictionary.js
@@ -0,0 +1,254 @@
+var dictionary = {
+  "WINGS FOUNDATION": {
+    en: "WINGS FOUNDATION",
+    ru: "ФОНД WINGS"
+  },
+  "Developers": {
+    en: "Developers",
+    ru: "Разработчики"
+  },
+  "Developer 1": {
+    en: "Developer 1",
+    ru: "Разработчик 1"
+  },
+  "Developer 2": {
+    en: "Developer 2",
+    ru: "Разработчик 2"
+  },
+  "Comm 1": {
+    en: "Comm 1",
+    ru: "Комм 1"
+  },
+  "Comm 2": {
+    en: "Comm 2",
+    ru: "Комм 2"
+  },
+  "Community": {
+    en: "Community",
+    ru: "Сообщество"
+  },
+  "Fly With Us": {
+    en: "Fly With Us",
+    ru: "Лети с нами"
+  },  
+  "Crowd powered evaluation, funding promotion, and early adopter engagement for ICOs": {
+    en: "Crowd powered evaluation, funding promotion, and early adopter engagement for ICOs",
+    ru: "Оценка, основанная на толпе, содействие и участие в раннем ICO"
+  },
+  "SUBSCRIBE": {
+    en: "SUBSCRIBE",
+    ru: "ПОДПИСКА"
+  },
+  "WINGS’ mission is the promotion of new, open decentralized technology architectures development and applications. In particular the promotion and maintenance of the WINGS protocol and the corresponding technology, as well as the promotion and support of applications using the protocol.": {
+    en: "WINGS’ mission is the promotion of new, open decentralized technology architectures development and applications. In particular the promotion and maintenance of the WINGS protocol and the corresponding technology, as well as the promotion and support of applications using the protocol.",
+    ru: "Миссия WINGS - продвижение новых, открытых децентрализованных технологических архитектурных разработок и приложений. В частности, продвижение и поддержание протокола WINGS и соответствующей технологии, а также продвижение и поддержка приложений, использующих протокол."
+  },
+    "Stay up to date with our latest development": {
+    en: "Stay up to date with our latest development",
+    ru: "Будьте в курсе новейшей разработки"
+  },
+  "THE TEAM": {
+    en: "THE TEAM",
+    ru: "КОМАНДА"
+  },
+  "WINGS Founders": {
+    en: "WINGS Founders",
+    ru: "WINGS Учредители"
+  },
+  "Tom Sawyer": {
+    en: "Tom Sawyer",
+    ru: "Том Сойер"
+  },
+  "founder &amp; ceo": {
+    en: "founder &amp; ceo",
+    ru: "Учредитель и CEO"
+  },
+  "Ivan Gogol": {
+    en: "Ivan Gogol",
+    ru: "Иван Гоголь"
+  },
+  "Sam Bodin": {
+    en: "Sam Bodin",
+    ru: "Сэм Бодин"
+  },
+  "THE SOMETHING": {
+    en: "THE SOMETHING",
+    ru: "THE Что-то"
+  },
+  "WINGS Advisors": {
+    en: "WINGS Advisors",
+    ru: "WINGS Советники"
+  },
+  "HOW IT WORKS": {
+    en: "HOW IT WORKS",
+    ru: "КАК РАБОТАЕТ"
+  },
+  "Applications": {
+    en: "Applications",
+    ru: "Приложения"
+  },
+  "ICO secure contract generator": {
+    en: "ICO secure contract generator",
+    ru: "ICO контрактный генератор"
+  },
+  "WINGS Protocol": {
+    en: "WINGS Protocol",
+    ru: "WINGS Протокол"
+  },
+  "Early Adopter Engagement": {
+    en: "Early Adopter Engagement",
+    ru: "Участие раннего приятеля"
+  },
+  "ICO Social Promotion": {
+    en: "ICO Social Promotion",
+    ru: "Соц Продвижение ICO"
+  },
+  "ICO Price Discovery": {
+    en: "ICO Price Discovery",
+    ru: "ICO Исследование Цен"
+  },
+  "LOREM IPSUM": {
+    en: "LOREM IPSUM",
+    ru: "ТЕКСТ РЫБА"
+  },
+  "Projects with WINGS": {
+    en: "Projects with WINGS",
+    ru: "Проекты с WINGS"
+  },
+  "Tokenbox is a unique ecosystem that brings together cryptocurrency fund under…": {
+    en: "Tokenbox is a unique ecosystem that brings together cryptocurrency fund under…",
+    ru: "Tokenbox - экосистема, которая объединяет cryptocurrency фонд под ..."
+  },
+  "Median forecast:": {
+    en: "Median forecast:",
+    ru: "Средний прогноз:"
+  },
+  "Amount raised:": {
+    en: "Amount raised:",
+    ru: "Сумма поднята:"
+  },
+  "Application layer": {
+    en: "Application layer",
+    ru: "Уровень приложения"
+  },
+  "Business logic layer": {
+    en: "Business logic layer",
+    ru: "Бизнес логики уровень"
+  },
+  "Infrastructure layer": {
+    en: "Infrastructure layer",
+    ru: "Уровень инфраструктуры"
+  },
+  "curation, price discovery, ICO contract, social promotion, bounties": {
+    en: "curation, price discovery, ICO contract, social promotion, bounties",
+    ru: "кураторство, ценовое открытие, контракт ICO, социальное продвижение, щедрость"
+  },
+  "Immutable algorithmic incentives, forecasting and promotion meritocracies, liquid democracy, DAO governance, ICO smart contract wizard, smart contract": {
+    en: "Immutable algorithmic incentives, forecasting and promotion meritocracies, liquid democracy, DAO governance, ICO smart contract wizard, smart contract",
+    ru: "Неизбежные алгоритмические стимулы, прогнозирование и поощрение заслуг, ликвидная демократия, управление DAO, интеллектуальный контракт ICO, умный контракт"
+  },
+  "Ethereum, ERC20, IPFS, Angular2.0, Node.JS, PostgreSQL": {
+    en: "Ethereum, ERC20, IPFS, Angular2.0, Node.JS, PostgreSQL",
+    ru: "Ethereum, ERC20, IPFS, Angular2.0, Node.JS, PostgreSQL"
+  },
+  "ABOUT US": {
+    en: "ABOUT US",
+    ru: "О НАС"
+  },
+  "About WINGS Stiftung": {
+    en: "About WINGS Stiftung",
+    ru: "Информация о WINGS Stiftung"
+  },
+  "Vision": {
+    en: "Vision",
+    ru: "Видение"
+  },
+  "Foundation Governance": {
+    en: "Foundation Governance",
+    ru: "Управление фондом"
+  },
+  "WINGS Stiftung also referred to as WINGS Foundation, is a not-for-profit foundation headquartered in Zug, Switzerland, exists to fulfill the WINGS Vision statement. Zug offers a neutral and internationally connected with strong fiduciary controls and a well functioning legal system.": {
+    en: "WINGS Stiftung also referred to as WINGS Foundation, is a not-for-profit foundation headquartered in Zug, Switzerland, exists to fulfill the WINGS Vision statement. Zug offers a neutral and internationally connected with strong fiduciary controls and a well functioning legal system.",
+    ru: "WINGS Stiftung, также называемый Фондом WINGS, является некоммерческим фондом со штаб-квартирой в Цуге, Швейцария, для выполнения заявления WINGS Vision. Цуг предлагает нейтральную и международную связь с сильным фидуциарным контролем и хорошо функционирующей правовой системой."
+  },
+  "Tokenization is the future, unprecedented global opportunity for the next generation of work - virtual economies that span human interests (vs geographic association)": {
+    en: "Tokenization is the future, unprecedented global opportunity for the next generation of work - virtual economies that span human interests (vs geographic association)",
+    ru: "Токенизация - это будущее, беспрецедентные глобальные возможности для следующего поколения работы - виртуальные экономики, которые охватывают интересы человека (против географической ассоциации)."
+  },
+  "WINGS Stiftung is managed by a Foundation Council who delegates decisions to committees, management and advisors.": {
+    en: "WINGS Stiftung is managed by a Foundation Council who delegates decisions to committees, management and advisors.",
+    ru: "WINGS Stiftung управляется Советом Фонда, который делегирует решения комитетам, руководству и советникам."
+  },
+  "The Foundation Council is to decides how to deploy funds donated to the Foundation. In order to pursue its R&amp;D ; objective the Board engages bought externally and internally sourced developers and organizations. Activities such as communications and advocacy rely on internal resources as well as the WINGS DAO stakeholder community.": {
+    en: "The Foundation Council is to decides how to deploy funds donated to the Foundation. In order to pursue its R&amp;D ; objective the Board engages bought externally and internally sourced developers and organizations. Activities such as communications and advocacy rely on internal resources as well as the WINGS DAO stakeholder community.",
+    ru: "Совет Фонда должен принять решение о том, как размещать средства, пожертвованные Фонду. Для продолжения исследований и разработок R&amp;D; Цель Совета заключается в приобретении внешних и внутренних поставщиков и организаций. Такие мероприятия, как коммуникации и информационно-пропагандистская деятельность, основаны на внутренних ресурсах, а также на сообществе заинтересованных сторон WINGS DAO."
+  },
+  "Mutara is the statutory auditor of the WINGS Foundation.": {
+    en: "Mutara is the statutory auditor of the WINGS Foundation.",
+    ru: "Мутара является уставным аудитором Фонда WINGS."
+  },
+  "Contact Us": {
+    en: "Contact Us",
+    ru: "Связаться с нами"
+  },
+  "Send": {
+    en: "Send",
+    ru: "Отправить"
+  },
+  "Join the conversation": {
+    en: "Join the conversation",
+    ru: "Присоединиться к разговору"
+  },
+  "© Copyright WINGS Stiftung 2018. All Rights Reserved.": {
+    en: "© Copyright WINGS Stiftung 2018. All Rights Reserved.",
+    ru: "© Copyright WINGS Stiftung 2018. Все права защищены."
+  },
+  "Blog": {
+    en: "Blog",
+    ru: "Блог"
+  },
+  "Whitepaper": {
+    en: "Whitepaper",
+    ru: "Белая бумага"
+  },
+  "Scietific whitepaper": {
+    en: "Scietific whitepaper",
+    ru: "Научный техдок"
+  },
+  "We are hiring": {
+    en: "We are hiring",
+    ru: "Мы нанимаем"
+  },
+  "Gubbelstasse 11, 6300 Zug, Switzerland": {
+    en: "Gubbelstasse 11, 6300 Zug, Switzerland",
+    ru: "Gubbelstasse 11, 6300 Цуг, Швейцария"
+  },
+  "Your Email Address": {
+    en: "Your Email Address",
+    ru: "Ваш Емейл"
+  },
+  "Mission": {
+    en: "Mission",
+    ru: "Миссия"
+  },
+  "First name": {
+    en: "First name",
+    ru: "Имя"
+  },
+  "Last name": {
+    en: "Last name",
+    ru: "Фамилия"
+  },
+  "Your email": {
+    en: "Your email",
+    ru: "Емейл"
+  },
+  "Phone number": {
+    en: "Phone number",
+    ru: "Телефон"
+  },
+  "Your mesage": {
+    en: "Your mesage",
+    ru: "Сообщение"
+  }
+}
