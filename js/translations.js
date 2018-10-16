3358ae808571b74361fd8e6dd161c928a3439641
Israel Levin <israel@paket.global>
2018-10-16 11:58:43 +0300

    Hierarchic translations

--- js/translations.js
+++ js/translations.js
@@ -1,5 +1,9 @@
 var translations = {
     en: {},
+    pl: {
+        pagetitle: 'This is in Polish',
+        credotitle: 'This is in Polish too'
+    },
     he: {
         pagetitle: 'אנחנו פאקט',
         credo: 'ואללה ואללה'
@@ -7,20 +11,26 @@ var translations = {
 };
 
 $(document).ready(function(){
-    $.each(translations, function(lang, translation){
-        opt = new Option(lang, lang);
-        $('#langselect').append(opt);
+    $.each(translations, function(langName){
+        optionElement = new Option(langName, langName);
+        $('#langselect').append(optionElement);
     });
-    $('#langselect').change(function(ev){
-        $.each(translations[ev.target.value], function(key, value){
-            var el = $('#' + key);
-            if(el.length === 0){
-                return true;
-            }
-            if(!(key in translations.en)){
-                translations.en[key] = el.text();
-            }
-            el.text(value);
+    $('#langselect').change(function(changeEvent){
+        var langCascade = ['en'];
+        if(changeEvent.target.value !== 'en'){
+            langCascade.push(changeEvent.target.value);
+        }
+        $.each(langCascade, function(idx, langName){
+            $.each(translations[langName], function(key, value){
+                var el = $('#' + key);
+                if(el.length === 0){
+                    return true;
+                }
+                if(!(key in translations.en)){
+                    translations.en[key] = el.text();
+                }
+                el.text(value);
+            });
         });
     });
 });
