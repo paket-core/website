cecc1803157c7bc326160472c75ddf194f96709d
Israel Levin <israel@paket.global>
2018-10-23 19:36:36 +0300

    Quotes script
    And some minor fixes, including auto run of ecosystem.

--- js/ecosystem.js
+++ js/ecosystem.js
@@ -5,17 +5,27 @@
         $(this).css({
             'transform': 'rotate(' + degrees + 'deg)'
         });
+        return $(this);
     };
 
     $(document).ready(function(){
         let ecosystem = $('#ecoGraph');
-        let currentRotation = 0;
+        let timeout, selected = 0;
         let items = ecosystem.find('.graph-item').click(function(){
-            let degrees = $(this).attr('data-pos') * 60;
+            let degrees, item = $(this);
+            clearTimeout(timeout);
+            selected = item.attr('data-pos');
+            degrees = selected * 60;
+            item.one('transitionend', function(){
+                $('#ecoGraphDesc h2').text(item.find('.graph-title').text());
+                $('#ecoGraphDesc p').text(item.find('.graph-desc').text());
+                clearTimeout(timeout);
+                timeout = setTimeout(function(){
+                    items.eq((parseInt(selected, 10) + 1) % 6).click();
+                }, 5000);
+            });
             ecosystem.rotate(degrees);
             items.rotate(-degrees);
-            $('#ecoGraphDesc h2').text($(this).find('.graph-title').text());
-            $('#ecoGraphDesc p').text($(this).find('.graph-desc').text());
         });
         items.last().click();
     });
